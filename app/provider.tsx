874e74037a47d31acd12f3f545d3298ff02e4d86
Manish Vishwakarma <122515021+HeaLthyDrugs@users.noreply.github.com>
2024-11-17 16:25:37 +0530

    Update provider.tsx just for casting

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+// casting numeric types with the other types with other sums
+
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
